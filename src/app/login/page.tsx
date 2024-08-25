a86c0a076a129431dd6a92cd893b0c6b3d45d289
Ricardo Jericevich <rickyjericevich@gmail.com>
2024-08-25 11:33:07 +0000

    Improved styles in globals.css and the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,44 +7,48 @@ import FormSubmitButton from '@/app/components/FormSubmitButton';
 export default function LoginPage() {
   const [loginErrorMessage, formAction] = useFormState(authenticate, undefined);
 
-  return <main>
+  return (
+    <main className="bg-gradient-to-b from-transparent to-background-end-rgb">
+      <form action={formAction}>
+        <h1 className="text-2xl mb-6 text-center text-gray-900 dark:text-gray-100">
+          Please log in to continue
+        </h1>
 
-    <form action={formAction}>
+        <label htmlFor="email" className="mb-4">
+          <span className="block">Email</span>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Enter your email address"
+            required
+            className="mt-1 block w-full px-3 py-2 dark:border-gray-600 sm:text-sm"
+          />
+        </label>
 
-      <h1>
-        Please log in to continue.
-      </h1>
+        <label htmlFor="password" className="mb-6">
+          <span className="block">Password</span>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            required
+            minLength={8}
+            className="mt-1 block w-full px-3 py-2 dark:border-gray-600 sm:text-sm"
+          />
+        </label>
 
-      <label htmlFor="email">
-        Email
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="Enter your email address"
-          required
-        />
-      </label>
+        <FormSubmitButton className="w-full border border-transparent text-sm font-medium">
+          Log in
+        </FormSubmitButton>
 
-      <label htmlFor="password">
-        Password
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="Enter password"
-          required
-          minLength={8}
-        />
-      </label>
-
-      <FormSubmitButton>Log in</FormSubmitButton>
-      {loginErrorMessage && <>
-        <p>{loginErrorMessage}</p>
-        <p>Please try again.</p>
-      </>
-      }
-    </form>
-
-  </main>
+        {loginErrorMessage && (
+          <div className="mt-4 text-center form-error">
+            <h2>{loginErrorMessage} Please try again.</h2>
+          </div>
+        )}
+      </form>
+    </main>
+  );
 }
